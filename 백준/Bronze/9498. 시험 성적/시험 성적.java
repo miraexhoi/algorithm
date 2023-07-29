e6bee037dcd7909f2be2b4935812186056f3801b
최미래 <109408165+miraexhoi@users.noreply.github.com>
2023-07-29 23:39:00 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int score = in.nextInt();
+
+        in.close();
+
+        if (score >= 90) System.out.println("A");
+        else if (score >= 80) System.out.println("B");
+        else if (score >= 70) System.out.println("C");
+        else if (score >= 60) System.out.println("D");
+        else System.out.println("F");
+    }
+}
