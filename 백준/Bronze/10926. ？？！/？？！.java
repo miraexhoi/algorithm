41357d02fb5d2bbbb6dc821bb4358c0db5da9595
최미래 <109408165+miraexhoi@users.noreply.github.com>
2023-08-17 08:53:07 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println(str + "??!");
+    }
+}
